2f7936d2b2dd97f0f2c736b9eb272b11fc88a893
verlitas <55808608+verlitas@users.noreply.github.com>
2019-12-21 12:51:20 -0700

    reworking schema to fit critera better

--- models/workout.js
+++ models/workout.js
@@ -2,18 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    workoutName: {
-        type: String,
-    },
-    sets: {
-        type: Number,
-    },
-    reps: {
-        type: Number
-    },
-    weight: {
-        type: Number
-    }
+  
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
