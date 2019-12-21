33cfb708b571c58db06e0f3a3f93e5f065a61e08
verlitas <55808608+verlitas@users.noreply.github.com>
2019-12-21 12:49:37 -0700

    following activity 15 for structure

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     workoutName: {
         type: String,
-        trim: true
     },
     sets: {
         type: Number,
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+const db = require("./models")
 const app = express();
-
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useFindAndModify: false,
 useUnifiedTopology: true });
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"))
+app.get("/results", (req,res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err)
+    });
+});
+
+app.post("/submit", ({ body }, res) => {
+    db.Workout.create(body)
+    .then(({_id}) => db.Workout.findOneAndUpdate({}, {$push: { workouts: _id}}, {new: true}))
+    .then(dbWorkout => {
+        res.json(dbUser)
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`==> 🌎  Listening on port ${PORT}!`)
