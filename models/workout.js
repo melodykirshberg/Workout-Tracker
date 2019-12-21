57d02ad78e689884dd8268bff2938c0ac31aa6b8
verlitas <55808608+verlitas@users.noreply.github.com>
2019-12-21 13:20:18 -0700

    reworking code

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    workouts: [
+        {
+            type:{
+                type: String
+            },
+            name: {
+                type: String
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            }
+      }]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -10,28 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useFindAndModify: false,
-useUnifiedTopology: true });
-
-app.get("/results", (req,res) => {
-    db.Workout.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout)
-    })
-    .catch(err => {
-        res.json(err)
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-    db.Workout.create(body)
-    .then(({_id}) => db.Workout.findOneAndUpdate({}, {$push: { workouts: _id}}, {new: true}))
-    .then(dbWorkout => {
-        res.json(dbUser)
-    })
-    .catch(err => {
-        res.json(err);
-    });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+    useNewUrlParser: true, useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 app.listen(PORT, () => {
